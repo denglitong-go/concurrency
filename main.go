44d262d9b12f8cad456a20912dca7d80f13d9e90
Litong Deng <litong.deng@compass.com>
2023-02-21 20:58:39 +0800

    add: ShowCommunicateBySharingMemory
    Chnannels allow you to pass references to data structures between goroutines.
    If you consider this as passing the ownership of the data (ability to read and write it),
    they become a powerful and expressive synchronization mechinism.
    
    Signed-off-by: Litong Deng <litong.deng@compass.com>

--- main.go
+++ main.go
@@ -26,4 +26,6 @@ func main() {
 	software.ShowConcurrentSearch()
 	software.ShowConcurrentSearchWithTimeout()
 	software.ShowConcurrentSearchAvoidTimeoutReduceTailLatency()
+
+	ShowCommunicateBySharingMemory()
 }
